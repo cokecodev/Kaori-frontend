eee8e1e72fce4082455f39ca66acb997c8360d73
cokecode <you@example.com>
2022-12-14 06:26:07 +0800

    [update] styled : 新增 RWD 相關數值

--- src/constants/style.js
+++ src/constants/style.js
@@ -13,3 +13,15 @@ export const COLOR = {
   gray_background:'#ebeade',
   gray_light:'#c5c4b6',
 }
+
+const size = {
+  mobile: '414px',
+  desktop: '768px'
+}
+
+export const MEDIA_QUERY = {
+  desktop: `@media screen and (min-width: ${size.desktop})`,
+  mobile: `@media screen and (max-width: ${size.desktop})`,
+  small_mobile:`@media screen and (max-width: ${size.mobile})`,
+  middle_breakpoint:`@media screen and (max-width: 545px)`
+}
