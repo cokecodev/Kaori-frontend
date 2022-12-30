782b338213705da60aa139fcab14c8344521cf02
cokecode <you@example.com>
2022-12-31 07:00:19 +0800

    [update] Login: component 整理、共用 style 抽出、切版優化ver1-a

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,66 +1,9 @@
-import styled from "styled-components"
-import { COLOR } from "../../constants/style"
+import { FloatCardWrapper, PageTitle, InputWrapper, LinkWrapper, LoginButton } from './styleForComponent'
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
 import { login, selectUser } from "../../features/userReducer"
 import useInput from '../../hooks/useInput'
-
-const Wrapper = styled.div`
-  width: 300px;
-  height: 300px;
-  margin: 100px auto;
-  padding: 30px;
-  transition: all 0.5s;
-
-  &:hover {
-    border: 1px solid ${COLOR.color3};
-    border-radius: 24px;
-    box-shadow: 4px 4px #ebeade;
-    background: white;
-  }
-
-  & form {
-    margin: 0 auto;
-    text-align: center;
-  }
-
-  & button {
-    margin: 50px 0;
-    width: 85%;
-    padding: 10px 0;
-    background: white;
-    color:${COLOR.color2};
-    border: 1px solid  ${COLOR.color2};
-    border-radius: 10px;
-  }
-
-  & button:hover {
-    background: ${COLOR.color3};
-    color:white;
-  }
-`
-const InputItem = styled.div`
-  margin: 0 auto;
-
-  & input {
-    padding: 6px 0; 
-    margin: 28px 0  0 8px  ;
-    border-radius: 4px;
-    border: 1px solid gray;
-  }
-`
-const PageTitle = styled.div`
-  font-size: 40px;
-  font-weight: 400;
-  color: ${COLOR.text_dark};
-  
-  display: flex;
-  justify-content: center;
-  margin: 10px 0;
-  padding-bottom: 24px;
-`
-
 
 export default function Login() {
   const {value: username, handleChange: handleUsernameChange} = useInput()
@@ -77,31 +20,40 @@ export default function Login() {
 
   const handleLoginSubmit = (e) => {
     e.preventDefault()
+    if(!username || !password ) return alert('請完整填寫!')
     const payload = {
       username,
       password
     }
-    dispatch(login(payload))
-    return navigate(-1)
+
+    dispatch(login(navigate, payload))
   }
 
   return (
-    <Wrapper>
-      <PageTitle> LOGIN</PageTitle>
-      <form onSubmit={handleLoginSubmit} >
-        <InputItem>
-          Username:
-          <input type='text' value={username} onChange={handleUsernameChange } />
-        </InputItem>
-        <InputItem>
-          Password: 
-          <input type='password' value={password} onChange={handlePasswordChange } />
-        </InputItem>
-          { /*
-            errorMessage && <Error>{errorMessage}</Error>
-          */}
-          <button>登入</button>
+    <FloatCardWrapper>
+      <PageTitle>LOGIN</PageTitle>
+      <form onSubmit = { handleLoginSubmit } >
+        <InputWrapper className = 'username'>
+          <input
+            type = 'text'
+            value = { username }
+            onChange = { handleUsernameChange }
+            placeholder = '帳號'
+            />
+        </InputWrapper>
+        <InputWrapper className = 'password'>
+          <input
+            type = 'password'
+            value = { password }
+            onChange = { handlePasswordChange }
+            placeholder = '密碼'
+          />
+        </InputWrapper>
+
+        <LinkWrapper><Link to='/register' >還沒有帳號? 前往註冊</Link></LinkWrapper>
+        <LoginButton>登入</LoginButton>
+
       </form>
-    </Wrapper>
-  );
+    </FloatCardWrapper>
+  )
 }
---
+++ src/components/Login/styleForComponent.js
@@ -0,0 +1,70 @@
+import styled from "styled-components"
+import { COLOR } from "../../constants/style"
+
+export const FloatCardWrapper = styled.div`
+  width: 300px;
+  margin: 100px auto;
+  padding: 30px;
+  transition: all 0.5s;
+
+  &:hover {
+    border: 1px solid ${COLOR.color3};
+    border-radius: 24px;
+    box-shadow: 4px 4px #ebeade;
+    background: white;
+  }
+
+  & form {
+    margin: 0 auto;
+    text-align: center;
+  }
+`
+export const PageTitle = styled.div`
+  font-size: 40px;
+  font-weight: 400;
+  color: ${COLOR.text_dark};
+  
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+  padding-bottom: 20px;
+`
+
+export const InputWrapper = styled.div`
+  margin: 0 auto;
+
+  & input {
+    width: 80%;
+    padding: 8px; 
+    margin-top: 28px;
+    border-radius: 6px;
+    border: 1px solid gray;
+  }
+
+  & input::placeholder {
+    color: gray;
+    padding: 4px;
+  }
+`
+export const LinkWrapper = styled.div`
+  margin-top: 30px;
+  
+  & a {
+    font-weight: 550;
+  }
+`
+export const LoginButton = styled.button`
+  margin: 30px 0;
+  width: 85%;
+  padding: 10px 0;
+  background: white;
+  color:${COLOR.color2};
+  border: 1px solid  ${COLOR.color2};
+  border-radius: 10px;
+  transition: all 0.3s;
+
+  &:hover {
+    background: ${COLOR.color3};
+    color: white;
+  }
+`
