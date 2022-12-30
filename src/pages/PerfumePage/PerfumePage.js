141d9a85b47e602a4dab6bfc6bbfb9a51d579606
cokecode <you@example.com>
2022-12-31 07:36:53 +0800

    [add] 新增 PerfumePage

---
+++ src/pages/PerfumePage/PerfumePage.js
@@ -0,0 +1,11 @@
+import Comments from "../../components/Comments"
+import PerfumeMain from "../../components/PerfumeMain"
+
+export default function PerfumePage() {
+  return (
+    <>
+      <PerfumeMain />
+      <Comments />
+    </>
+  );
+}
