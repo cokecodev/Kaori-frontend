b617eb2f00c547568a53ad6be3baa09e1ade5c54
cokecode <you@example.com>
2023-01-10 05:38:08 +0800

    [update] perfumeMain: navigate debug (about login) and fix typo

--- src/components/PerfumeMain/PerfumeMain.js
+++ src/components/PerfumeMain/PerfumeMain.js
@@ -2,15 +2,23 @@ import { useParams } from "react-router-dom"
 import useGetOnePerfume from '../../hooks/perfumeHooks/useGetPerfumeData'
 import PerfumeBasicInfo from './PerfumeBasicInfo'
 import PerfumeShowVoteData from'./PerfumeShowVoteData'
-import { useEffect, useMemo } from "react"
+import Vote from'../Vote'
+import BlockPartition from '../BlockPartition'
+
+import { useEffect, useState } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { getMe, selectUser } from '../../features/userReducer'
 
 export default function PerfumeMain() {
+  const dispatch = useDispatch()
+  const currentUser = useSelector(selectUser)
   const perfumeId = Number(useParams().id)
+  const [isVoteHidden, setIsVoteHidden] = useState(true)
   const {
     perfume,
     fetchError,
     creator,
-    brand, 
+    brand,
     ingredient,
     voteData,
     getPerfumeFetch,
@@ -18,11 +26,38 @@ export default function PerfumeMain() {
     booleanVoteData,
     getBooleanVoteFetch,
   } = useGetOnePerfume(perfumeId)
+  
+  useEffect(() => {
+    if (currentUser) return
+    dispatch(getMe())
+    console.log('yo ')
+  },[currentUser, dispatch])
+  
+  // TODO: render 問題
+  //console.log('page perfumeMain',voteData)
+  //console.log('currentUser', currentUser)
 
-  console.log('page perfumeMain',voteData)
+  const handleToggleHidden = () => {
+    setIsVoteHidden(!isVoteHidden)
+  }
+
+  const handelOpenVoteAreaClick = () => {
+    if(!currentUser) return alert('請先登入!')
+    handleToggleHidden()
+  }
+
 
   return (
     <>
+    { currentUser !=='' && <Vote
+        currentUser = { currentUser }
+        ingredient = { ingredient }
+        perfumeId = { perfumeId }
+        isVoteHidden = { isVoteHidden }
+        setIsVoteHidden = { setIsVoteHidden }
+        handleToggleHidden = { handleToggleHidden }
+      />
+    }
       <PerfumeBasicInfo
         brand = { brand }
         perfume = { perfume }
@@ -31,12 +66,18 @@ export default function PerfumeMain() {
         booleanVoteData = { booleanVoteData }
       />
 
+      <BlockPartition 
+        subtitle = {'投票去'} 
+        handleClick = { handelOpenVoteAreaClick } 
+      >
+        VOTES
+      </BlockPartition>
+
       <PerfumeShowVoteData
         voteData = { voteData }
         ingredient = { ingredient }
       />
 
     </>
-  );
- 
+  )
 }
