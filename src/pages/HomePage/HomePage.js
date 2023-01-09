cf8e0794ecaf8c438e93f38cbe67be07e5f0fbd5
cokecode <you@example.com>
2023-01-10 05:56:49 +0800

    [add] 新增 HomePage

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import Banner from "../../components/Banner"
+import HomePageCardsSection from "../../components/HomePageCardsSection"
+
+export default function HomePage() {
+
+  return (
+    <>
+      <Banner/>
+      <HomePageCardsSection/>
+    </>
+  )
+}
