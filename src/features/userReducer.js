8ea913812dd3fdbe32e1dbb629b19bdfbc2643d7
cokecode <you@example.com>
2023-01-10 05:33:22 +0800

    [update] userReduscer: navigate bug fixed

--- src/features/userReducer.js
+++ src/features/userReducer.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { loginApi, logoutApi, getMeApi } from '../WebAPI'
+import { loginApi, logoutApi, getMeApi, registerApi } from '../WebAPI'
 
 export const userSlice = createSlice({
   name: 'user',
@@ -24,14 +24,15 @@ export const selectUser = (state) => state.user.currentUser;
 export default userSlice.reducer;
 
 // TODO: 錯誤處理?
-export const login = (payload) => (dispatch) => {
+export const login = (navigate, payload) => (dispatch) => {
   loginApi(payload).then(res => {
     // TODO: 錯誤訊息要顯示在哪?
     if(res.data.ok !==1) return alert(res.data.message)
     const data = { 
       currentUser: res.data.data,
     }
-    dispatch(setMe(data))
+    //dispatch(setMe(data))
+    navigate(-1)
   })
 
 }
@@ -53,4 +54,18 @@ export const getMe = () => (dispatch) => {
 
     dispatch(setMe(data))
   })
+}
+
+export const register = (navigate, payload) => (dispatch) => {
+  registerApi(payload).then(res => {
+    // TODO: 錯誤訊息要顯示在哪?
+    if(res.data.ok !==1) return alert(res.data.message)
+    const data = { 
+      currentUser: res.data.data,
+    }
+    
+    //dispatch(setMe(data)) // 後端的緣故所以如果直接跳轉就不需要再 set me 一次了
+    navigate('/')
+  })
+
 }
