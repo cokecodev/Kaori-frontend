b26eccef09ff19f8f6def3fb1f96931f38f9b7e1
cokecode <you@example.com>
2023-02-21 07:43:02 +0800

    [update] component Banner: 新增 search 相關切版、顯示邏輯 ( 搜尋功能 : 相關邏輯待完成 )

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,92 @@
 import styled from "styled-components"
-import { COLOR } from '../../constants/style'
+import useInput from '../../hooks/useInput'
 
-const BannerWrapper =styled.div`
-  background: gray;
-  height: 450px;
+
+const BannerWrapper = styled.div`
+  background: ${props => props.imgName ? `url(/Banner/${props.imgName}.jpg) center/cover no-repeat` : 'gray' };
+  height: 580px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+`
+const BannerTitle = styled.div`
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+  color:${props => props.titleColor ? `${props.titleColor}` : 'black' };
+`
+const SearchBar = styled.input`
+  border-radius: 8px;
+  outline: none;
+  border: none;
+  padding: 8px 10px;
+  min-width: 250px;
+
+  ::placeholder { 
+    color: #aeb6b8;
+    font-size: 8px;
+  }
 `
 
-export default function Banner() {
+
+const handlePerfumeSearchSubmit = (e) => {
+  e.preventDefault()
+  return alert('A submit !')
+}
+
+const handleBrandSearchSubmit = (e) => {
+  e.preventDefault()
+  return alert('B submit !')
+}
+
+const handleCreatorSearchSubmit = (e) => {
+  e.preventDefault()
+  return alert('C submit !')
+}
+
+
+export default function Banner({ searchType, title, titleColor, imgName }) {
+  const { value: searchValue, setValue: setSearchValue, handleChange: handleSearchBarChange } = useInput()
 
   return (
     <>
-      <BannerWrapper>
-        'YOYOYO BANNER'
+      <BannerWrapper imgName = { imgName } >
+        <div className = 'banner__item-container'>
+          <BannerTitle titleColor = { titleColor } >
+            { title }
+          </BannerTitle>
+          
+          { searchType ==='perfume' && (
+            <form onSubmit = { handlePerfumeSearchSubmit } >
+              <SearchBar
+                className = 'search__input'
+                placeholder = '搜尋香水'
+                onChange = { handleSearchBarChange }
+              />
+            </form>
+          )}
+
+          { searchType ==='brand' && (
+            <form onSubmit = { handleBrandSearchSubmit } >
+              <SearchBar
+                className = 'search__input'
+                placeholder = '搜尋品牌'
+                onChange = { handleSearchBarChange }
+              />
+            </form>
+          )}
+
+          { searchType ==='creator' &&(
+            <form onSubmit = { handleCreatorSearchSubmit } >
+              <SearchBar
+                className = 'search__input'
+                placeholder = '搜尋調香師'
+                onChange = { handleSearchBarChange }
+              />
+            </form>
+          )}
+          
+        </div>
       </BannerWrapper>
     </>
   )
