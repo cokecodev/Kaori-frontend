471ff3b229ef3962ed90a40f2ab34c6307d285b8
cokecode <you@example.com>
2023-02-27 10:05:35 +0800

    [add] 新增 CreatorListPage

---
+++ src/pages/CreatorListPage/CreatorListPage.js
@@ -0,0 +1,52 @@
+import styled from "styled-components"
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { getAllCreator, selectCreatorList } from "../../features/searchReducer"
+
+import Banner from '../../components/Banner'
+import CreatorInfoCard from '../../components/CreatorInfoCard'
+import { PageDescribeTitle } from '../../components/general'
+
+const TitleWithMargin = styled(PageDescribeTitle)`
+  margin-bottom: 50px;
+`
+
+export default function BrandPage() {
+  const creatorList = useSelector(selectCreatorList)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    dispatch(getAllCreator())
+  }, [])
+
+
+  return (
+    <>
+      <Banner
+        imgName = { 'C' }
+        titleColor = { 'white' }
+        title = { '來探索同頻的調香師吧 !' }
+        searchType = { 'creator' }
+      />
+
+
+      <TitleWithMargin> 調香師列表 </TitleWithMargin>
+      
+      { // creator cards
+        creatorList.length !== 0 && creatorList.map( res => { 
+          let path = `/creator/${res.id}`
+
+          return (
+            <Link to = { path } key = { res.id }>
+              <CreatorInfoCard
+                creator = { res } 
+              />
+            </Link>
+          ) 
+        })
+      }
+
+    </>
+  )
+}
