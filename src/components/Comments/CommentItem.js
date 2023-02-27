4e75363066497e8685dbc2332685f9ad5a67b579
cokecode <you@example.com>
2023-02-27 10:18:41 +0800

    [update] CSS微修: avator相關、部分樣式共用 export

--- src/components/Comments/CommentItem.js
+++ src/components/Comments/CommentItem.js
@@ -4,38 +4,48 @@ import { MEDIA_QUERY } from "../../constants/style"
 
 import CreateEditButtons from "./CreateEditButtons"
 
-const CommentWrapper = styled(BigCardWrapper)`
+export const CommentWrapper = styled(BigCardWrapper)`
   display: flex;
   justify-content: space-between;
   position: relative;
 `
-const Avatar = styled.div`
+export const Avatar = styled.div`
   background: rgba(0, 0, 0, 0.1);
   height: 100px;
   width: 100px;
   border-radius: 50%;
+  overflow: hidden;
+
+  & img {
+    width: 100%;
+  }
+
+  ${MEDIA_QUERY.mobile} {
+    height: 70px;
+    width: 70px;
+  }
 `
-const DataArea = styled.div`
+export const DataArea = styled.div`
   flex: 1;
   text-align: left;
   margin-left: 30px;
   font-family: 微軟正黑體;
 `
-const AuthorDataWrapper = styled.div`
+export const AuthorDataWrapper = styled.div`
   padding-bottom: 0.5rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `
-const LeftPartWrapper = styled.div`
+export const LeftPartWrapper = styled.div`
   display: flex;
   align-items: center;
 
-  ${MEDIA_QUERY.mobile}{
+  ${MEDIA_QUERY.mobile} {
     display: block;
   }
 `
-const Author = styled.div`
+export const Author = styled.div`
   font-size: 1.3rem;
   margin-right: 1rem;
 
@@ -58,7 +68,7 @@ export default function CommentItem ({comment, handleCommentDelete, currentUser,
   return(
     <CommentWrapper>
       <Avatar>
-        <div/>
+        <img className = 'photo__url'/>
       </Avatar>
       
       <DataArea>
