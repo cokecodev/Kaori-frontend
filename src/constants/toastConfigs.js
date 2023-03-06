f6ebed7ae19fdbbeacbfc2d497218ed90c31f999
cokecode <you@example.com>
2023-03-06 21:33:05 +0800

    [add] 新增 toastify 設定檔、 userReducer : 成功及錯誤顯示改用 toastify

---
+++ src/constants/toastConfigs.js
@@ -0,0 +1,11 @@
+import { toast } from 'react-toastify'
+
+export const toastConfig = {
+  autoClose: 2000,
+  theme: 'light'
+}
+
+export const errorToastConfig = {
+  autoClose: false,
+  theme: 'colored',
+}
--- src/features/userReducer.js
+++ src/features/userReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { loginApi, logoutApi, getMeApi, registerApi } from '../WebAPI'
+import { toast } from 'react-toastify'
+import { toastConfig, errorToastConfig } from '../constants/toastConfigs'
 
 export const userSlice = createSlice({
   name: 'user',
@@ -23,49 +25,62 @@ export const { setLogout, setMe } = userSlice.actions;
 export const selectUser = (state) => state.user.currentUser;
 export default userSlice.reducer;
 
-// TODO: 錯誤處理?
 export const login = (navigate, payload) => (dispatch) => {
   loginApi(payload).then(res => {
-    // TODO: 錯誤訊息要顯示在哪?
-    if(res.data.ok !==1) return alert(res.data.message)
+    if(res.data.ok !==1) return toast.warn(res.data.message, toastConfig)
     const data = { 
       currentUser: res.data.data,
     }
-    //dispatch(setMe(data))
-    navigate(-1)
+    
+    dispatch(setMe(data))
+    toast.success('登入成功', toastConfig)
+    return navigate(-1)
+  }).catch(err => {
+    console.log('ERR', err.message.toString())
+    toast.error(err.message, errorToastConfig)
   })
-
 }
 
 export const logout = () => (dispatch) => {
   logoutApi().then(res => {
-    // TODO: 錯誤訊息要顯示在哪?
-    if(res.data.ok !==1) return alert('登出出了點錯呢!')
+    if(res.data.ok !==1) return toast.error('登出出了點錯呢!', errorToastConfig)
+    
     dispatch(setLogout())
+    toast.success('登出成功', toastConfig)
+  }).catch(err => {
+    console.log('ERR', err.message.toString())
+    toast.error(err.message, errorToastConfig)
   })
 }
 
 export const getMe = () => (dispatch) => {
   getMeApi().then(res => {
     if(res.data.ok !==1) return dispatch(setMe(''))
+    
     const data = { 
       currentUser: res.data.data,
     }
 
     dispatch(setMe(data))
+  }).catch(err => {
+    console.log('ERR', err.message.toString())
+    toast.error(err.message, errorToastConfig)
   })
 }
 
 export const register = (navigate, payload) => (dispatch) => {
   registerApi(payload).then(res => {
-    // TODO: 錯誤訊息要顯示在哪?
-    if(res.data.ok !==1) return alert(res.data.message)
+    if(res.data.ok !==1) return toast.warn(res.data.message, toastConfig)
     const data = { 
       currentUser: res.data.data,
     }
     
-    //dispatch(setMe(data)) // 後端的緣故所以如果直接跳轉就不需要再 set me 一次了
+    dispatch(setMe(data))
     navigate('/')
+    toast.success('註冊成功', toastConfig)
+  }).catch(err => {
+    console.log('ERR', err.message.toString())
+    toast.error(err.message, errorToastConfig)
   })
 
 }
