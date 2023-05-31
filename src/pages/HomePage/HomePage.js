2bfffb1b78612c9f9cf8a4579057d829b17a7f37
cokecode <you@example.com>
2023-05-31 15:09:03 +0800

    [update] HomePage 畫面優化: 新增 PromoteBlock

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,14 +4,12 @@ import { toastConfig } from '../../constants/toastConfigs'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectIsLoading, selectFetchError, setIsLoading, setFetchError } from '../../features/fetchStatusReducer'
 
-import { getLatestFivePerfume } from '../../WebAPI'
+import { getAllPerfume } from '../../WebAPI'
 import { GeneralPageWrapper } from '../../components/general'
 import Banner from '../../components/Banner'
-import PageDescribeTitle from '../../components/PageDescribeTitle'
-import HomePageCardsSection from '../../components/HomePageCardsSection'
 import Loading from '../../components/Loading'
 import ErrorMessage from '../../components/ErrorMessage'
-
+import PromoteBlock from '../../components/PromoteBlock/PropmoteBlock'
 
 export default function HomePage() {
   const dispatch = useDispatch()
@@ -19,11 +17,11 @@ export default function HomePage() {
   const isLoading = useSelector(selectIsLoading)
   const fetchError = useSelector(selectFetchError)
 
-  const getLatestFiveFetch = () => {
+  const getPerfumeFetch = () => {
     dispatch(setIsLoading(true))
     dispatch(setFetchError(null))
 
-    getLatestFivePerfume()
+    getAllPerfume()
       .then(res => {
         if(res.data.ok !== 1) toast.warn(res.data.message, toastConfig)
         setPerfumes(res.data.data)
@@ -37,7 +35,7 @@ export default function HomePage() {
   }
 
   useEffect(() => {
-    getLatestFiveFetch()
+    getPerfumeFetch()
   },[])
 
   return (
@@ -52,13 +50,38 @@ export default function HomePage() {
           searchType = 'perfume'
         />
 
-        <PageDescribeTitle
-          title = '最新加入的五款香水'
+        <PromoteBlock
+          ClassName ='latest3'
+          imgName ='1'
+          title ='給喜歡新鮮感的你'
+          context ='為您展示本平台最新上架的香水們，隆底家辣！快來加入我們討論的行列，不管是文青木質調、費洛蒙菸草調還是清新青草調，都在等你來探索！'
+          perfumes = { perfumes?.slice(-3) }
         />
         
-        { // perfume cards
-          perfumes.length !== 0 && <HomePageCardsSection perfumes = { perfumes } />
-        }
+        <PromoteBlock
+          className ='石內卜推薦'
+          imgName ='2'
+          title ='給喜歡探險的你'
+          context ='才華洋溢的石內卜教授，日前盛大推出的新品們！在魔藥學領域有極高的造詣，擅長使用各種特殊的材料，創造出麻瓜界前所未見的奇妙味道。'
+          perfumes = { perfumes?.filter((perfume) => perfume.creatorId === 4).slice(0,3) }
+        />
+        
+        <PromoteBlock
+          className ='木質調推薦'
+          imgName ='3'
+          title ='熱門的木質調'
+          context = '特別獻給喜歡木質調的木男木女們，木質調神通廣大，有揉合果香、花香、西普或東方調等等衍伸範疇，等你來發掘 !'
+          perfumes = { perfumes?.filter((perfume) => perfume.group === '木質調').slice(-3) }
+        />
+        
+        <PromoteBlock
+          className ='上架最久的三款'
+          imgName ='4'
+          title ='給隨意晃晃的你'
+          context ='本區囊括了本平台最經典的幾款香氣，諸位香氛控們走過不要路過阿！相信絕對能讓你找到更多關於香氛的靈感RRR'
+          perfumes = { perfumes?.slice(0,3) }
+        />
+
       </GeneralPageWrapper>
     </>
   )
